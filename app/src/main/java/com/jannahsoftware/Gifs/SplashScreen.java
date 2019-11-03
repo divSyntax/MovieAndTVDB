51a3fb4292d821c18a74b6a42a04a0fdf547a4de
DivSyntax <divsyntax@gmail.com>
2019-11-03 13:39:16 -0600

    Added gif splashscreen

---
+++ app/src/main/java/com/jannahsoftware/Gifs/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.jannahsoftware.Gifs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.jannahsoftware.moviedb.MainActivity;
+import com.jannahsoftware.moviedb.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run()
+            {
+                Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        },5000);
+    }
+}
